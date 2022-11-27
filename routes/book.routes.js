9e8a5fa2ae3adaaa824caa7dd17ae92bdc792e6d
Elidiane Bezerra <elidiane.asb@gmail.com>
2022-11-27 17:03:55 +0000

    created function to check account type

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const Book = require('../models/Book.model');
+const User = require('../models/User.model');
 const Service = require('../models/Service.model')
 // Require necessary (isAuthenticated) middleware in order to control access to specific routes
 const { isAuthenticated } = require("../middleware/jwt.middleware.js");
-const { Types, model } = require("mongoose");
+const { Types } = require("mongoose");
 
 router.post('/books', isAuthenticated, (req, res, next) => {
     const { name, local, date, contact, service } = req.body;
@@ -14,19 +15,22 @@ router.post('/books', isAuthenticated, (req, res, next) => {
         .catch(err => res.json(err));
 })
 
-router.get('/books', (req, res, next) => {
-    Book.find()
-        .populate('service')
-        .then(allBooks => res.json(allBooks))
-        .catch(err => res.json(err));
-})
-
-router.get('/books/:userId', isAuthenticated, (req, res, next) => {
-    const userId = req.params.userId
-    console.log(userId)
-    Book.find({user: Types.ObjectId(userId)})
-        .populate('service')
-        .then(allBooks => res.json(allBooks))
+router.get('/books', isAuthenticated, (req, res, next) => {
+    const authenticatedUser = req.payload._id
+    User.findById(authenticatedUser)
+        .then(user => {
+            if (user.accountType === "client") {
+                Book.find({ user: Types.ObjectId(authenticatedUser) })
+                    .populate('service')
+                    .then(allBooks => res.json(allBooks))
+                    .catch(err => res.json(err));
+            } else if (user.accountType === "admin") {
+                Book.find()
+                    .populate('service')
+                    .then(allBooks => res.json(allBooks))
+                    .catch(err => res.json(err));
+            }
+        })
         .catch(err => res.json(err));
 })
 
